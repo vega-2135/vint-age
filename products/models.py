73e6a306ca8d0a67e66186f69259a61538031ca4
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-05-09 15:31:19 +0200

    Add sizes to product model and box to select size in product_detail site
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

--- products/models.py
+++ products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
+    
     class Meta:
         verbose_name_plural = 'Categories'
         
@@ -20,6 +21,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     rating = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
