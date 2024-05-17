8fea39b0b56997dcf79ac701ae925ea4cfa3e403
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-05-17 11:58:51 +0200

    Prepare for deployment
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vintage.wsgi:application
--- vintage/settings.py
+++ vintage/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
@@ -28,7 +29,7 @@ SECRET_KEY = "django-insecure-z^sdczb_bbzf-9jvvz@j682$16^0654gpwtqba%6!6#f67!a6%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["vint-age.herokuapp.com", "localhost"]
 
 
 # Application definition
@@ -122,13 +123,17 @@ WSGI_APPLICATION = "vintage.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}
-
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
