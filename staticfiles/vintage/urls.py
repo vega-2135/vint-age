705f3628794627c843b363fb99c9e5de8c622dcb
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-05-22 15:02:29 +0200

    Add homepage image to static files
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

---
+++ staticfiles/vintage/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
+    path("", include("home.urls")),
+    path("products/", include("products.urls")),
+    path("bag/", include("bag.urls")),
+    path("checkout/", include("checkout.urls")),
+    path("profile/", include("profiles.urls")),
+    path("contact/", include("contact.urls")),
+    path("faq/", include("faq.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
