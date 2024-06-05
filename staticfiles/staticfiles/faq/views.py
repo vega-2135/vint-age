e105890201a9a277b4d8eb062cb8a6d8edfa785e
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-06-05 15:10:06 +0200

    Fix the staticfiles directory
    Previously it had a nested copy of the complete repo (twice).
    This in turn made heroku fail due to timeout during the collectstatic
    step. The way I found out was by adding the DISABLE_COLLECTSTATIC flag,
    and having the build fail due to the slug size being bigger than the
    limit. While debugging that I realized that I mistakenly added this
    directory which had much more than the generated static assets directory
    hierarchy.
    
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

--- staticfiles/staticfiles/faq/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-from .models import Question
-
-
-def faq(request):
-    questions = Question.objects.all()
-    return render(request, "faq/faq.html", {"questions": questions})
